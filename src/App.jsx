dbbaea6f7add3fdbeeec4715abba7c29218e32e4
zidol <ajh3166@naver.com>
2025-08-09 17:39:27 +0900

    useEffect 라이프사이클 제어

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(`count: ${count} / input: ${input}`);
+  }, [count, input]);
+  //의존성 배열
+  //dependency array
+  //deps
+
   const onclickButton = (value) => {
     setCount(count + value);
   };
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        ></input>
+      </section>
       <section>
         <Viewer count={count} />
       </section>
